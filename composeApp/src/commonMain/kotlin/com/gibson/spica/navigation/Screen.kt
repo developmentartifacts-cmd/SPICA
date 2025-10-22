1121056df8489ee8f3b444baedee44fafe2aec8d
developmentartifacts-cmd <developmentartifacts@gmail.com>
2025-10-22 09:35:31 +0100

    Update Screen.kt

--- composeApp/src/commonMain/kotlin/com/gibson/spica/navigation/Screen.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/spica/navigation/Screen.kt
@@ -1,1 +1,32 @@
+package com.gibson.spica.navigation
 
+/**
+ * Represents all navigable routes in SPICA.
+ * Used by Router and NavigationModel.
+ */
+sealed class Screen(val route: String) {
+    object Signup : Screen("signup")
+    object Login : Screen("login")
+    object AccountSetup : Screen("account_setup")
+    object EmailVerify : Screen("email_verify")
+    object PhoneVerify : Screen("phone_verify")
+    object Home : Screen("home")
+    object Marketplace : Screen("marketplace")
+    object Portfolio : Screen("portfolio")
+    object Watchlist : Screen("watchlist")
+
+    companion object {
+        fun fromRoute(route: String?): Screen? = when (route) {
+            Signup.route -> Signup
+            Login.route -> Login
+            AccountSetup.route -> AccountSetup
+            EmailVerify.route -> EmailVerify
+            PhoneVerify.route -> PhoneVerify
+            Home.route -> Home
+            Marketplace.route -> Marketplace
+            Portfolio.route -> Portfolio
+            Watchlist.route -> Watchlist
+            else -> null
+        }
+    }
+}
