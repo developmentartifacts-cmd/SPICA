94baf7af6ff37341d87841c39f7aab974b1e874c
developmentartifacts-cmd <developmentartifacts@gmail.com>
2025-10-24 20:07:30 +0100

    Remove AuthService initialization in MainActivity
    Removed unused AuthService initialization from MainActivity.

--- composeApp/src/androidMain/kotlin/com/gibson/spica/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/spica/MainActivity.kt
@@ -8,18 +8,12 @@ import com.gibson.spica.navigation.AppNavigation
 import com.google.firebase.FirebaseApp
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.gibson.spica.ui.theme.SpicaTheme
-import com.gibson.spica.data.AuthService
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         FirebaseApp.initializeApp(this)
         FirebaseAnalytics.getInstance(this)
-
-        AuthService.currentActivity = this
-        AuthService.handleInitialRoute()
-
         setContent {
             SpicaTheme(isDarkTheme = isSystemInDarkTheme()) {
                 AppNavigation()
