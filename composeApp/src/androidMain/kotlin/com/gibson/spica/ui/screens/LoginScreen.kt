98caf3fedf7c51e4838920a10c565f585c508185
developmentartifacts-cmd <developmentartifacts@gmail.com>
2025-10-25 21:19:36 +0100

    Remove duplicate import of KeyboardOptions

--- composeApp/src/androidMain/kotlin/com/gibson/spica/ui/screens/LoginScreen.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/spica/ui/screens/LoginScreen.kt
@@ -1,12 +1,12 @@
 package com.gibson.spica.ui.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.unit.dp
 import com.google.firebase.auth.FirebaseAuth
 import com.gibson.spica.navigation.Router
