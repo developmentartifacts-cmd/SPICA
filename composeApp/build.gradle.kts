407f285c354df00249af35ed4b767e5908929197
developmentartifacts-cmd <developmentartifacts@gmail.com>
2025-10-26 10:11:12 +0100

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -59,6 +59,7 @@ kotlin {
             implementation(compose.material3)
             implementation(compose.materialIconsExtended)
             implementation(compose.ui)
+            implementation("com.squareup.okio:okio:3.9.0")
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
