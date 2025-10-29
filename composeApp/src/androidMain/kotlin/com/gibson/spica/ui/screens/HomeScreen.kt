0970f97237c9bc0443a1ceddb7d911bb0d4bc8f0
developmentartifacts-cmd <developmentartifacts@gmail.com>
2025-10-29 09:18:08 +0100

    Update HomeScreen.kt

--- composeApp/src/androidMain/kotlin/com/gibson/spica/ui/screens/HomeScreen.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/spica/ui/screens/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.gibson.spica.ui.screens
 
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.unit.dp
 import com.gibson.spica.ui.components.SpicaFrame
