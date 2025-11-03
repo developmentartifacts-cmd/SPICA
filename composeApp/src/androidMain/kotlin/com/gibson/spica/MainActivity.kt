2740d2441be87655f48d83e0c3de9c93e5c0c357
developmentartifacts-cmd <developmentartifacts@gmail.com>
2025-11-04 00:15:45 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/gibson/spica/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/spica/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
 import com.gibson.spica.navigation.AppNavigation
+import com.gibson.spica.navigation.Router
+import com.gibson.spica.navigation.Screen
 import com.google.firebase.FirebaseApp
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.gibson.spica.ui.theme.SpicaTheme
@@ -14,6 +16,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         FirebaseApp.initializeApp(this)
         FirebaseAnalytics.getInstance(this)
+
+        val startRoute = intent.getStringExtra("startRoute") ?: Screen.Splash.route
+        Router.resetTo(startRoute)
+
         setContent {
             SpicaTheme(isDarkTheme = isSystemInDarkTheme()) {
                 AppNavigation()
