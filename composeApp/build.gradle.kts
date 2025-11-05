0260b8c74c08fde191bbacfb4f01447437e57bf4
developmentartifacts-cmd <developmentartifacts@gmail.com>
2025-11-05 12:03:34 +0100

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -61,7 +61,7 @@ kotlin {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
-            implementation("network.chaintech:cmp-country-code-picker:1.0.1")
+           
             implementation(compose.materialIconsExtended)
             implementation(compose.ui)
             implementation(compose.components.resources)
