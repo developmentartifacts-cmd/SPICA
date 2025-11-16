da99236aee19b8a418e3631c7eec5e453a18819c
developmentartifacts-cmd <developmentartifacts@gmail.com>
2025-11-16 10:35:10 +0100

    Update SignupScreen.kt

--- composeApp/src/androidMain/kotlin/com/gibson/spica/ui/screens/SignupScreen.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/spica/ui/screens/SignupScreen.kt
@@ -1,5 +1,6 @@
 package com.gibson.spica.ui.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
