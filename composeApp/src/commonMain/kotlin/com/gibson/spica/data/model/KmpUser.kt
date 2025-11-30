667d54dff1e93db3ad00d2e43c86c9e3b9323ae8
developmentartifacts-cmd <developmentartifacts@gmail.com>
2025-11-30 16:08:47 +0100

    Delete composeApp/src/commonMain/kotlin/com/gibson/spica/data directory

--- composeApp/src/commonMain/kotlin/com/gibson/spica/data/model/KmpUser.kt
+++
@@ -1,9 +0,0 @@
-package com.gibson.spica.data.model
-
-data class KmpUser(
-    val uid: String,
-    val email: String?,
-    val isEmailVerified: Boolean,
-    val displayName: String?,
-    val photoUrl: String?
-)
