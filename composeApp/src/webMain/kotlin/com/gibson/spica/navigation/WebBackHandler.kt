06347a90e64c9197f7de909339c26e7aca1bc08c
developmentartifacts-cmd <developmentartifacts@gmail.com>
2025-12-01 08:56:20 +0100

    Create WebBackHandler.kt

---
+++ composeApp/src/webMain/kotlin/com/gibson/spica/navigation/WebBackHandler.kt
@@ -0,0 +1,8 @@
+package com.gibson.spica.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun BackHandler(enabled: Boolean, onBack: () -> Unit) {
+    // Optional: integrate with browser history API later
+}
