f908677d24d0e0fe15d3b990e5b954cec9da96cf
developmentartifacts-cmd <developmentartifacts@gmail.com>
2025-12-01 08:38:40 +0100

    Update App.kt

--- composeApp/src/commonMain/kotlin/com/gibson/spica/App.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/spica/App.kt
@@ -1,9 +1,15 @@
 package com.gibson.spica
 
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import com.gibson.spica.navigation.AppNavigation
+import com.gibson.spica.navigation.AppNavHost
+import com.gibson.spica.ui.theme.SpicaTheme
 
 @Composable
 fun App() {
-    AppNavigation()
+    SpicaTheme {
+        Surface {
+            AppNavHost()
+        }
+    }
 }
